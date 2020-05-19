79ba3023b7225bc8c1dcf8cf40e93e3ddea353f9
YgorRegato <58819037+YgorRegato@users.noreply.github.com>
2020-05-19 17:26:36 -0300

    solucionando bug

--- classes/ClassLayout.php
+++ classes/ClassLayout.php
@@ -87,7 +87,7 @@ class ClassLayout{
         $html = "
         </main>
             <!-- Footer -->
-            <footer class='page-footer font-small color-footer lighten-3 pt-4'>
+            <footer class='page-footer font-small style-footer lighten-3 pt-4'>
         
               <!--Imagens-->
               <div class='container'>
@@ -105,20 +105,19 @@ class ClassLayout{
                     </ul>
                 </div>
         
-                <div class='row'>
+                <div class='row grid-footer'>
         
-        
-                  <div class='col-lg-2 col-md-12 mb-4'>
-                    <div class='view overlay z-depth-1-half'>
-                      <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg' class='img-fluid'
-                        alt=''>
-                      <a href=''>
-                        <div class='mask rgba-white-light'></div>
-                      </a>
-                    </div>
+                <div class='col-lg-2 col-md-6 mb-4 edit-width'>
+                  <div class='view overlay z-depth-1-half'>
+                    <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).jpg' class='img-fluid'
+                      alt=''>
+                    <a href=''>
+                      <div class='mask rgba-white-light'></div>
+                    </a>
                   </div>
+                </div>
         
-                  <div class='col-lg-2 col-md-6 mb-4'>
+                  <div class='col-lg-2 col-md-6 mb-4 edit-width'>
                     <div class='view overlay z-depth-1-half'>
                       <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).jpg' class='img-fluid'
                         alt=''>
@@ -128,7 +127,7 @@ class ClassLayout{
                     </div>
                   </div>
         
-                  <div class='col-lg-2 col-md-6 mb-4'>
+                  <div class='col-lg-2 col-md-6 mb-4 edit-width'>
                     <div class='view overlay z-depth-1-half'>
                       <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).jpg' class='img-fluid'
                         alt=''>
@@ -137,19 +136,20 @@ class ClassLayout{
                       </a>
                     </div>
                   </div>
-        
-                  <div class='col-lg-2 col-md-12 mb-4'>
-                    <div class='view overlay z-depth-1-half'>
-                      <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).jpg' class='img-fluid'
-                        alt=''>
-                      <a href=''>
-                        <div class='mask rgba-white-light'></div>
-                      </a>
-                    </div>
-        
+
+                  <div class='col-lg-2 col-md-6 mb-4 edit-width'>
+                  <div class='view overlay z-depth-1-half'>
+                    <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).jpg' class='img-fluid'
+                      alt=''>
+                    <a href=''>
+                      <div class='mask rgba-white-light'></div>
+                    </a>
                   </div>
+                </div>
         
-                  <div class='col-lg-2 col-md-6 mb-4'>
+                  
+        
+                  <div class='col-lg-2 col-md-6 mb-4 edit-width'>
                       <div class='view overlay z-depth-1-half'>
                         <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).jpg' class='img-fluid'
                           alt=''>
@@ -159,7 +159,7 @@ class ClassLayout{
                       </div>
                   </div>
         
-                  <div class='col-lg-2 col-md-6 mb-4'>
+                  <div class='col-lg-2 col-md-6 mb-4 edit-width'>
                     <div class='view overlay z-depth-1-half'>
                       <img src='https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(84).jpg' class='img-fluid'
                         alt=''>
