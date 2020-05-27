400ae4070dfec3a92df82e2df456ebe5a49bf254
Renan Pereira <renansoaresinfo@gmail.com>
2020-05-26 21:47:06 -0300

    Cria validação de e-mail e data

--- classes/ClassValidate.php
+++ classes/ClassValidate.php
@@ -5,7 +5,7 @@ namespace Classes;
 class ClassValidate{
     private $erro=[];
 
-    //Validar se os campos desejados foram preenchidos.
+    #Valida se os campos desejados foram preenchidos.
     public function validateFields($par){
         $i=0;
         foreach ($par as $key => $value){
@@ -22,6 +22,27 @@ class ClassValidate{
         }
     }
 
+    #Valida se o dado é um e-mail
+    public function validateEmail($par){
+        if (filter_var($par,FILTER_VALIDATE_EMAIL)){
+            return true;
+        }else{
+            $this->setErro("E-mail inválido.");
+            return false;
+        }
+    }
+
+    #Valida se o dado é uma data
+    public function validateData($par){
+        $data= \DateTime::createFromFormat("d/m/Y",$par);
+        if(($data) && ($data->format("d/m/Y") === $par)){
+            return true;
+        }else{
+            $this->setErro("Data inválida.");
+            return false;
+        }
+    }
+
     public function getErro(): array{
         return $this->erro;
     }
@@ -29,4 +50,6 @@ class ClassValidate{
     public function setErro($erro){
         array_push($this->erro,$erro);
     }
+
+
 }
--- controllers/controllerCadastro.php
+++ controllers/controllerCadastro.php
@@ -2,5 +2,7 @@
 $validate = new \Classes\ClassValidate();
 
 $validate->validateFields($_POST);
+$validate->validateEmail($_POST['email']);
+$validate->validateData($_POST['dataNascimento']);
 
 var_dump($validate->getErro());
