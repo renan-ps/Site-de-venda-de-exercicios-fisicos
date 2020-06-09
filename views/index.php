24e518d81762a2bb95c9032723f75cb585f44bb6
joalisonferreira <32495993+joalisonferreira@users.noreply.github.com>
2020-06-09 20:31:54 -0300

    correção faq

--- views/index.php
+++ views/index.php
@@ -601,7 +601,7 @@
             <div class="card-header border-bottom border-light bg-warning" role="tab" id="headingOne1">
               <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true"
                 aria-controls="collapseOne1">
-                <h5 class="text-white font-weight-normal btn-warning">
+                <h5 class="text-white font-weight-normal mb-2 mt-2">
                   Is this a secure site for purchases? <i class=" float-right fas fa-angle-down rotate-icon"></i>
                 </h5>
               </a>
@@ -625,7 +625,7 @@
             <div class="card-header border-bottom border-light bg-warning" role="tab" id="headingTwo2">
               <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2"
                 aria-expanded="false" aria-controls="collapseTwo2">
-                <h5 class="text-white font-weight-normal mb-2 mt-2 btn-warning">
+                <h5 class="text-white font-weight-normal mb-2 mt-2 ">
                   How long are your contracts? <i class="float-right  fas fa-angle-down rotate-icon"></i>
                 </h5>
               </a>
