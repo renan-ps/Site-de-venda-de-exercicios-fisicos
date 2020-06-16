d24906b55f76ccc84a7b226b3481189e1a2534d2
joalisonferreira <32495993+joalisonferreira@users.noreply.github.com>
2020-06-16 20:37:23 -0300

    Pagina Planos, e atualização nas paginas de usuarios

--- views/user/Planos.php
+++ views/user/Planos.php
@@ -4,15 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Blank Page - Brand</title>
-    <link rel="stylesheet" href="../../lib/css/user/bootstrap.min.css?h=504792f7254b7c7ba831bdd93d286139">
+    <title>Planos</title>
+  <link rel="stylesheet" href="../../lib/css/user/bootstrap.min.css?h=504792f7254b7c7ba831bdd93d286139">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-  
-    
-    <link rel="stylesheet" href="../../lib/css/user/Timeline.css">
-  
+   
+    <link rel="stylesheet" href="../../lib/css/user/clean-responsive-pricing.css?h=03147876505551d80bce125ae9d141b1">
 </head>
 
 <body id="page-top">
@@ -20,21 +17,12 @@
         <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
             <div class="container-fluid d-flex flex-column p-0">
                 <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-                    <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink fas fa-dumbbell"></i></div>
+                    <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
                     <div class="sidebar-brand-text mx-3"><span>treino do zé</span></div>
                 </a>
                 <hr class="sidebar-divider my-0">
                 <ul class="nav navbar-nav text-light" id="accordionSidebar">
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="Planos.php"><i class="far fa-user-circle"></i><span>Gerenciar Plano</span></a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link" href="Perfil_user.php"><i class="fas fa-user-circle"></i><span>Minhas Informaçoes</span></a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="time_line_user.php">
-                        <svg class="bi bi-person-lines-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
-</svg>
-                        <span>TimeLine</span></a></li>
-                    <li class="nav-item" role="presentation"></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link " href="#"><i class="fas fa-window-maximize"></i><span>Gerenciar Pagamentos</span></a></li>
-                  
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="Planos.php"><i class="fas fa-window-maximize"></i><span>Gerenciar Plano</span></a></li>
                 </ul>
                 <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
             </div>
@@ -43,11 +31,6 @@
             <div id="content">
                 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
                     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
-                        <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                            <div class="input-group">
-                                <div class="input-group-append"></div>
-                            </div>
-                        </form>
                         <ul class="nav navbar-nav flex-nowrap ml-auto">
                             <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><i class="fas fa-search"></i></a>
                                 <div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" role="menu" aria-labelledby="searchDropdown">
@@ -104,7 +87,7 @@
                                             </div>
                                         </a>
                                         <a class="d-flex align-items-center dropdown-item" href="#">
-                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="../../image/avatars/avatar2.jpeg?h=5d142be9441885f0935b84cf739d4112">
+                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="assets/img/avatars/avatar2.jpeg?h=5d142be9441885f0935b84cf739d4112">
                                                 <div class="status-indicator"></div>
                                             </div>
                                             <div class="font-weight-bold">
@@ -113,7 +96,7 @@
                                             </div>
                                         </a>
                                         <a class="d-flex align-items-center dropdown-item" href="#">
-                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="../../image/avatars/avatar3.jpeg?h=c5166867f10a4e454b5b2ae8d63268b3">
+                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="assets/img/avatars/avatar3.jpeg?h=c5166867f10a4e454b5b2ae8d63268b3">
                                                 <div class="bg-warning status-indicator"></div>
                                             </div>
                                             <div class="font-weight-bold">
@@ -122,7 +105,7 @@
                                             </div>
                                         </a>
                                         <a class="d-flex align-items-center dropdown-item" href="#">
-                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="../../image/avatars/avatar5.jpeg?h=35dc45edbcda6b3fc752dab2b0f082ea">
+                                            <div class="dropdown-list-image mr-3"><img class="rounded-circle" src="assets/img/avatars/avatar5.jpeg?h=35dc45edbcda6b3fc752dab2b0f082ea">
                                                 <div class="bg-success status-indicator"></div>
                                             </div>
                                             <div class="font-weight-bold">
@@ -135,7 +118,7 @@
                             </li>
                             <div class="d-none d-sm-block topbar-divider"></div>
                             <li class="nav-item dropdown no-arrow" role="presentation">
-                                <div class="nav-item dropdown no-arrow"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color: rgb(62,100,211);"><span class="d-none d-lg-inline mr-2 text-gray-600 small">Valerie Luna</span><img class="border rounded-circle img-profile" src="../../image/avatars/avatar1.jpeg?h=0ecc82101fb9a10ca459432faa8c0656"></button>
+                                <div class="nav-item dropdown no-arrow"><button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button"><span class="d-none d-lg-inline mr-2 text-gray-600 small">Valerie Luna</span><img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg?h=0ecc82101fb9a10ca459432faa8c0656"></button>
                                     <div
                                         class="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu"><a class="dropdown-item" role="presentation" href="#"><i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Profile</a><a class="dropdown-item" role="presentation" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Settings</a>
                                         <a
@@ -146,98 +129,83 @@
                     </ul>
             </div>
             </nav>
+            <div class="container-fluid">
+                <h3 class="text-dark mb-1" style="margin-top: -16px;">Meu Plano</h3>
+            </div>
             <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <!-- Start: Timeline -->
-                        <section id="section-timeline">
-                            <div class="container" style="margin-top: -113px;">
-                                <div class="row">
-                                    <div class="col-lg-12 text-center">
-                                        <h2 class="section-heading">TimeLine</h2>
-                                        <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur. </h3>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <ul class="list-group timeline">
-                                            <li class="list-group-item">
-                                                <div class="timeline-image"><img class="rounded-circle img-fluid" src="../../image/1.jpg?h=45a9ad06704f106dd3e235cd887192c8"></div>
-                                                <div class="timeline-panel">
-                                                    <div class="timeline-heading">
-                                                        <h4>2009-2011 </h4>
-                                                        <h4 class="subheading">Our Humble Beginnings </h4>
-                                                    </div>
-                                                    <div class="timeline-body">
-                                                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium
-                                                            consectetur! </p>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li class="list-group-item timeline-inverted">
-                                                <div class="timeline-image"><img class="rounded-circle img-fluid" src="../../image/2.jpg?h=208f4ea26a9bebc6eecbc302d5fc16b7"></div>
-                                                <div class="timeline-panel">
-                                                    <div class="timeline-heading">
-                                                        <h4>March 2011 </h4>
-                                                        <h4 class="subheading">An Agency is Born </h4>
-                                                    </div>
-                                                    <div class="timeline-body">
-                                                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium
-                                                            consectetur! </p>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li class="list-group-item">
-                                                <div class="timeline-image"><img class="rounded-circle img-fluid" src="../../image/3.jpg?h=9e18e850d271345b653fdfb5f39beea9"></div>
-                                                <div class="timeline-panel">
-                                                    <div class="timeline-heading">
-                                                        <h4>December 2012 </h4>
-                                                        <h4 class="subheading">Transition to Full Service </h4>
-                                                    </div>
-                                                    <div class="timeline-body">
-                                                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium
-                                                            consectetur! </p>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li class="list-group-item timeline-inverted">
-                                                <div class="timeline-image"><img class="rounded-circle img-fluid" src="../../image/4.jpg?h=11d30dfdbf0ac580625cd899e1572d8d"></div>
-                                                <div class="timeline-panel">
-                                                    <div class="timeline-heading">
-                                                        <h4>July 2014 </h4>
-                                                        <h4 class="subheading">Phase Two Expansion </h4>
-                                                    </div>
-                                                    <div class="timeline-body">
-                                                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium
-                                                            consectetur! </p>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li class="list-group-item timeline-inverted">
-                                                <div class="timeline-image">
-                                                    <h4>Be Part Of Our Story!</h4>
-                                                </div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                        <!-- End: Timeline -->
+                <!-- Start: NMDIG Jumbotron Basic 2 Actions -->
+                <section>
+                    <div class="jumbotron">
+                        <h1 class="text-center" style="margin-top: -14px;margin-bottom: 11px;">Ooops!</h1>
+                        <h1 class="text-center">Nenhum Plano Ativo</h1>
+                        <p class="text-center">Assine um plano e começe uma vida mais saudavel</p>
+                        <p class="text-center"><a class="btn btn-primary" role="button" href="#">Ver Planos</a><br><a class="btn btn-link btn-sm" role="button" href="#">Cancelar</a></p>
                     </div>
-                </div>
+                </section>
+                <!-- End: NMDIG Jumbotron Basic 2 Actions -->
+            </div>
+            <div class="container">
+                <!-- Start: clean responsive pricing --><div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>20 users included</li>
+              <li>10 GB of storage</li>
+              <li>Priority email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>30 users included</li>
+              <li>15 GB of storage</li>
+              <li>Phone and email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+                <!-- End: clean responsive pricing -->
             </div>
         </div>
         <footer class="bg-white sticky-footer">
             <div class="container my-auto">
-                <div class="text-center my-auto copyright"><span>Copyright © Brand 2019</span></div>
+                <div class="text-center my-auto copyright"><span>Copyright © CornOut 2019</span></div>
             </div>
         </footer>
     </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-    
     <script src="../../lib/js/theme.js" type="text/javascript" charset="utf-8"></script>
 </body>
 
