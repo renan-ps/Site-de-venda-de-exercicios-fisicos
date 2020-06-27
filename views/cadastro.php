b0d5b22fdff2365635d27457235842b5ea4c6c29
Renan Pereira <renansoaresinfo@gmail.com>
2020-06-27 00:23:56 -0300

    Corrige página de cadastro

--- views/cadastro.php
+++ views/cadastro.php
@@ -1,68 +1,95 @@
 <?php echo \Classes\ClassLayout::setHeaderUser('Cadastro', "Faça seu cadastro", ""); ?>
 
-<div id="demo" style="margin-left: 20px; padding-top: 20px;"><a href="personal.php" title=""><i class="fas fa-arrow-left fa-3x"></i></a></div>
 
 <style type="text/css" media="screen">
 	@media (min-width: 1200px) {
-		.bg  {
-			background-image:  url(<?php echo DIRIMG . '../image/Fundo-Login.svg'; ?>) ;
+		.bg {
+			background-image: url(<?php echo DIRIMG . '../image/Fundo-Login.png'; ?>);
 		}
 	}
 </style>
+<style>
+	.whatsapp {
+		position: fixed;
+		top: 72%;
+		left: 94%;
+		padding: 10px;
+		z-index: 10000000;
+	}
+</style>
+<div>
+	<a href="https://api.whatsapp.com/send?phone=55###########"
+	   target="_blank">
+		<img class="whatsapp" src="https://images.tcdn.com.br/static_inst/integracao/imagens/whatsapp.png"/>
+	</a>
+</div>
 
-<body class="bg">
-<div class="container" style="margin-top: -60px;">
-	<div class="card shadow-lg o-hidden border-0 my-5">
-		<div class="card-body p-0">
-			<div class="row">
-				<div class="col-lg-5 d-none d-lg-flex">
-					<div class="flex-grow-1 bg-register-image" style="background-image: url(<?php echo DIRIMG . '../image/img-carousel-2.jpg'; ?>);"></div>
+<body class="bg ">
+<div class="container ">
+	<div class="card shadow-lg  o-hidden border-0 div-cadastro  ">
+		<div class="card-body p-0 o">
+			<div class="row ">
+				<div class="col-lg-5 d-none d-lg-flex ">
+					<div class="flex-grow-1 bg-register-image"
+					     style="background-image: url(<?php echo DIRIMG . '../image/dogs/image1.jpeg'; ?>);"></div>
 				</div>
 				<div class="col-lg-7">
-					<div class="p-5">
+					<div class="p-2">
 						<div class="text-center">
+
 							<h4 class="text-dark mb-4">Crie sua Conta!</h4>
 						</div>
-						<form class="user" method="post" name="formCadastro" id="formCadastro" action="<?php echo DIRPAGE . 'controllers/controllerCadastro'; ?>">
-							<div class="form-group row">
 
-								<div class="col-sm-12 mb-3 mb-sm-0">
-									<input class="form-control form-control-user" type="text" id="nome" placeholder="Nome" name="nome" required="" onfocus="">
-								</div>
-
-
-							</div>
-
-							<div class="form-group">
-								<input class="form-control form-control-user" type="email" id="email"  placeholder="Email " name="email" required="">
-							</div>
+						<form class="user" method="post" name="formCadastro" id="formCadastro"
+						      action="<?php echo DIRPAGE . 'controllers/controllerCadastro'; ?>">
 
 							<div class="form-group row">
+
 								<div class="col-sm-6 mb-3 mb-sm-0">
 
-									<input class="form-control form-control-user" type="date" id="dataNascimento" placeholder="Data de Nascimento" name="dataNascimento" required="">
+									<input class="form-control form-control-user" type="text" id="nome" placeholder="Nome" name="nome"
+									>
 								</div>
 
 								<div class="col-sm-6">
-									<input class="form-control form-control-user" type="text" id="telefone" placeholder="Telefone" name="telefone">
+									<input class="form-control form-control-user" type="email" id="email" placeholder="E-mail"
+									       name="email">
 								</div>
 
 
 							</div>
-							<div class="form-group">
-								<input class="form-control form-control-user" type="text" id="endereco"  placeholder="Endereço " name="endereco" required="">
-							</div>
-
 
 
 							<div class="form-group row">
 								<div class="col-sm-6 mb-3 mb-sm-0">
 
-									<input class="form-control form-control-user" type="password" id="senha" placeholder="Senha" name="senha" required="">
+									<input class="form-control form-control-user" type="text" id="dataNascimento"
+									       placeholder="Data de Nascimento" name="dataNascimento">
 								</div>
 
 								<div class="col-sm-6">
-									<input class="form-control form-control-user" type="password" id="senhaConf" placeholder="Reescreva a senha" name="senhaConf" required="">
+									<input class="form-control form-control-user" type="text" id="telefone" placeholder="Telefone"
+									       name="telefone">
+								</div>
+
+
+							</div>
+							<div class="form-group">
+								<input class="form-control form-control-user" type="text" id="endereco" placeholder="Endereço "
+								       name="endereco">
+							</div>
+
+
+							<div class="form-group row">
+								<div class="col-sm-6 mb-3 mb-sm-0">
+
+									<input class="form-control form-control-user" type="password" id="senha" placeholder="Senha"
+									       name="senha">
+								</div>
+
+								<div class="col-sm-6">
+									<input class="form-control form-control-user" type="password" id="senhaConf"
+									       placeholder="Reescreva a senha" name="senhaConf">
 								</div>
 
 
@@ -70,13 +97,17 @@
 
 
 							<input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
-							<div class="" >
+							<div class="">
 								<div class="retornoCad"></div>
 							</div>
 
 
 							<button class="btn btn-warning btn-block text-white btn-user" type="submit">Criar Conta</button>
-							<hr><a class="btn btn-primary btn-block text-white btn-google btn-user" role="button"><i class="fab fa-google"></i>&nbsp; Criar com Google</a><a class="btn btn-primary btn-block text-white btn-facebook btn-user" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Criar com Facebook</a>
+							<hr>
+							<a class="btn btn-primary btn-block text-white btn-google btn-user" role="button"><i
+									class="fab fa-google"></i>&nbsp; Criar com Google</a><a
+								class="btn btn-primary btn-block text-white btn-facebook btn-user" role="button"><i
+									class="fab fa-facebook-f"></i>&nbsp; Criar com Facebook</a>
 							<hr>
 						</form>
 						<div class="text-center"><a class="small" href="user/reset_senha_user">Esqueceu a Senha ?</a></div>
@@ -91,7 +122,8 @@
 <script src='lib/js/sweetalert.min.js'></script>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js'></script>
-<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js'></script>
+<script type='text/javascript'
+        src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js'></script>
 <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.15.0/js/mdb.min.js'></script>
 <script src='lib/js/jquery.js'></script>
 <script src='lib/js/index.js'></script>
