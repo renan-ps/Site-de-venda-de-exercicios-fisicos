333748b27e5fd0c6ba4f7cd2b0127945f4ac38f4
YgorRegato <58819037+YgorRegato@users.noreply.github.com>
2020-07-07 22:49:34 -0300

    Criacao das paginas de Conta Cancelada e Recuperacao de Senha

---
+++ views/conta-cancelada.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=width-device, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/css/reset.css">
+    <link rel="stylesheet" href="lib/css/style.css">
+</head>
+
+
+<body class="bg-conta-cancelada body-conta-cancelada">
+    <div class="conta-cancelada-container">
+        <div class="img-conta-cancelada">
+            <img src="image/img-conta-encerrada-1.png" alt="Boneco branco com X nas mãos">
+        </div>
+        <div class="texto-conta-cancelada">
+            <h1>Conta cancelada.</h1>
+            <p>Sempre que precisar, estaremos aqui esperando por você!</p>
+        </div>
+    </div>
+</body>
+</html>
